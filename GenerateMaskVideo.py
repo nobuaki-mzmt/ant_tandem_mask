4645aba4bbaa0689361c10d6f935e23309199ac4
Nobuaki Mizumoto (水元 惟暁) <nobuaki.mzmt@gmail.com>
2023-05-18 14:01:05 +0900

    Implement gui

--- GenerateMaskVideo.py
+++ GenerateMaskVideo.py
@@ -3,19 +3,17 @@
 # Loading Libraries
 import cv2
 import sys
-import numpy as np
 import glob
-import csv
-import pprint
 import os
 import pandas as pd
+import numpy as np
+import PySimpleGUI as sg
+from tqdm import tqdm
 
 # ---------------------------------------------------------------------------------------------
 # Main part of video generation
 # ---------------------------------------------------------------------------------------------
-
-
-def VideoGeneration(idir, odir, tandem_windows_size = 20):
+def VideoGeneration(idir, tandem_windows_size):
 
     # Loading data
     video_path = glob.glob(idir + os.sep + "*.mp4")
@@ -41,9 +39,12 @@ def VideoGeneration(idir, odir, tandem_windows_size = 20):
         height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
         count = video.get(cv2.CAP_PROP_FRAME_COUNT)
         fps = video.get(cv2.CAP_PROP_FPS)
+        print(v)
         print("width:{}, height:{}, count:{}, fps:{}".format(width,height,count,fps))
+        # endregion ------
 
         # region ----- 2. Make background -----
+        print("Creating background...")
         has_next, i_frame = video.read()
 
         # background
@@ -66,50 +67,92 @@ def VideoGeneration(idir, odir, tandem_windows_size = 20):
             # next frame
             has_next, i_frame = video.read()
 
-        cv2.imwrite(idir + "/background.jpg", back_frame)
+        cv2.imwrite(idir + os.sep() + "background.jpg", back_frame)
+        # endregion ------
 
-        #########################
-        #### 3. Video Create ####
-        #########################
+        # region ----- 3. Video Create -----
+        print("Video generation")
         fourcc = cv2.VideoWriter_fourcc('m','p','4', 'v')
         writer = cv2.VideoWriter(v.replace('.mp4', '_extract.mp4'), fourcc, fps, (int(width), int(height)))
-        num = 0
         video.set(cv2.CAP_PROP_POS_FRAMES, 0) 
 
         video = cv2.VideoCapture(v)
 
-        while(video.isOpened()):
+        for num in tqdm(range(location.shape[0])):
             ret, frame = video.read()
-            if num < location.shape[0]:
-                x = int(location.xBody[num])
-                y = int(location.yBody[num])
+            x = int(location.xBody[num])
+            y = int(location.yBody[num])
 
-                #cv2.circle(frame, (x,y), 2, color=(0,0,255), thickness=-1)
-                bg = cv2.imread(idir + "/background.jpg")
-                cv2.rectangle(bg, (x-tandem_windows_size+1,y-tandem_windows_size+1), (x+tandem_windows_size-1, y+tandem_windows_size-1), (0,0,0), -1, cv2.LINE_4)
+            #cv2.circle(frame, (x,y), 2, color=(0,0,255), thickness=-1)
+            bg = cv2.imread(idir + os.sep() + "background.jpg")
+            cv2.rectangle(bg, (x-tandem_windows_size+1,y-tandem_windows_size+1), (x+tandem_windows_size-1, y+tandem_windows_size-1), (0,0,0), -1, cv2.LINE_4)
 
-                cv2.rectangle(frame, (1,1), (x-tandem_windows_size, int(height)), (0,0,0), -1, cv2.LINE_4)
-                cv2.rectangle(frame, (x+tandem_windows_size,1), (int(width), int(height)), (0,0,0), -1, cv2.LINE_4)
-                cv2.rectangle(frame, (1,1), (int(width), y-tandem_windows_size), (0,0,0), -1, cv2.LINE_4)
-                cv2.rectangle(frame, (1,y+tandem_windows_size), (int(width), int(height)), (0,0,0), -1, cv2.LINE_4)
+            cv2.rectangle(frame, (1,1), (x-tandem_windows_size, int(height)), (0,0,0), -1, cv2.LINE_4)
+            cv2.rectangle(frame, (x+tandem_windows_size,1), (int(width), int(height)), (0,0,0), -1, cv2.LINE_4)
+            cv2.rectangle(frame, (1,1), (int(width), y-tandem_windows_size), (0,0,0), -1, cv2.LINE_4)
+            cv2.rectangle(frame, (1,y+tandem_windows_size), (int(width), int(height)), (0,0,0), -1, cv2.LINE_4)
 
-                frame=cv2.add(bg,frame)
-                
-                ## plot
-                cv2.imshow('frame', frame)
-                cv2.waitKey(1)
+            frame=cv2.add(bg,frame)
+            
+            ## plot
+            #cv2.imshow('frame', frame)
+            #cv2.waitKey(1)
 
-                writer.write(frame)
-                #cv2.imwrite(HOME + "Plots/Visualize/p8-13/" + "picture{:0=5}".format(num)+".jpg", frame)
-                #print("save picture{:0=5}".format(num)+".jpg")
-
-            else:
-                break
-
-            num += 1
+            writer.write(frame)
+        # endregion ------
 
         cv2.destroyAllWindows()
         writer.release()
     cv2.destroyAllWindows()
 
-VideoGeneration(idir = "F:/Dropbox/research/papers_and_projects/2023/STARPROTOCOL/GenerateMaskVideo/sample", odir="")
+def gui():
+    sg.theme('Dark')
+    frame_file = sg.Frame('', [
+        [sg.Text("In   "),
+         sg.InputText('Input folder', enable_events=True, size=(25, 1)),
+         sg.FolderBrowse(button_text='select', size=(6, 1), key="-IN_FOLDER_NAME-")
+         ],
+        [sg.Text("Tandem window size (def = 20px):", size=(25,1)),
+         sg.In(key='-WINDOW_SIZE-', size=(6, 1))
+        ]
+    ], size=(300, 80))
+
+    frame_buttom = sg.Frame('', [
+        [sg.Submit(button_text='Start', size=(10, 10), key='START')]], 
+        size=(50, 50))
+    
+    layout = [[frame_file, frame_buttom]]
+    
+    window = sg.Window('Tandem masking',
+                       layout, resizable=True)
+    
+    while True:
+        event, values = window.read()
+    
+        if event is None:
+            print('exit')
+            break
+        else:
+            if event == 'START':
+
+                # file info
+                if len(values["-IN_FOLDER_NAME-"]) == 0:
+                    print("no input!")
+                    continue
+                else:
+                    in_dir = values["-IN_FOLDER_NAME-"]+os.sep
+                
+                # parameters
+                if len(values['-WINDOW_SIZE-']) == 0:
+                    tandem_windows_size = 20
+                else:
+                    tandem_windows_size = int(values["-WINDOW_SIZE-"])
+
+                print("input dir: "+str(in_dir))
+                message = VideoGeneration(in_dir, tandem_windows_size)
+                if message is not None:
+                    sg.popup(message)            
+    window.close()
+
+
+gui()
